00fb0e5814fea56b654e7240109239de0d99bad6
fernando duenas <fernando39615@aim.com>
2020-10-01 07:50:47 -0500

    third commit

--- color-combinator.js
+++ color-combinator.js
@@ -1,8 +1,22 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function colorCombinator(comboInput1, comboInput2) {
 
+  const validPrim1 = 'red';
+  const validPrim2 = 'yellow';
+  const validPrim3 = 'blue';
 
+  if((validPrim1 == comboInput1) && (validPrim2 == comboInput2)) {
+    return 'orange';
+  }
+  if((validPrim1 == comboInput1) && (validPrim3 == comboInput2)) {
+    return 'purple';
+  }
+  if((validPrim3 == comboIput1) && (validPrim2 == comboInput2)) {
+    return 'green';
+  }
+}
 
 
 
--- color-deconstructor.js
+++ color-deconstructor.js
@@ -1,8 +1,22 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function colorDeconstructor(validSecInput11){
 
+  const validSecond1 = 'orange';
+  const validSecond2 = 'purple';
+  const validSecond3 = 'green';
 
+  if(validSecond1 == validSecInput11) {
+    return 'red and yellow';
+  }
+  if(validSecond2 == validSecInput11) {
+    return 'red and blue';
+  }
+  if(validSecond3 == validSecInput11) {
+    return 'blue and yellow';
+  }
+}
 
 
 // Our setup code here. Don't touch!
